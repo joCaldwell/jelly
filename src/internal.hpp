3c1973e57b0aa0be3344942de375a3bdbdd5fff1
joCaldwell <joshcaldwell270@gmail.com>
2022-07-22 19:21:20 -0500

    inital commit

---
+++ src/internal.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include"../include/JLY/jelly.h"
---
+++ src/window.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <iostream>
+#include "internal.hpp"
+#include <glm/glm.hpp>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+void RenderWindow(){
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
+ 
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+ 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+ 
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+ 
+ 
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+    }
+    glfwSwapInterval(1);
+ 
+    // NOTE: OpenGL error checks have been omitted for brevity
+ 
+    while (!glfwWindowShouldClose(window))
+    {
+        int width,height;
+        glfwGetFramebufferSize(window, &width, &height);
+        float ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+ 
+    glfwDestroyWindow(window);
+ 
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+};
